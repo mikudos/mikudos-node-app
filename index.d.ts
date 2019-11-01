818024be301d59108d69cddf9ba2e548e4f5e87a
guanyu <yue.guanyu@hotmail.com>
2019-11-01 21:32:47 +0800

    update

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,21 @@
-import { Application } from './src';
+import Mali from 'mali';
 
-export default Application;
+export default class MikudosApp extends Mali {
+    public config: any;
+    public settings: any;
+    public context: any;
+    constructor(
+        path: any,
+        name?: string | ReadonlyArray<string>,
+        options?: any
+    );
+    get(name: string): any;
+    set(name: string, value: any): MikudosApp;
+    disable(name: string): MikudosApp;
+    disabled(name: string): boolean;
+    enable(name: string): MikudosApp;
+    enabled(name: string): boolean;
+    configure(fn: Function): MikudosApp;
+}
+
+export namespace mikudosApp {}
