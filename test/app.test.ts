1e17d6dd022ed449896dd94b75cac565de5e326f
guanyu <yue.guanyu@hotmail.com>
2020-01-07 18:41:08 +0800

    fix test address binding error

--- test/app.test.ts
+++ test/app.test.ts
@@ -4,4 +4,4 @@ import path from 'path';
 const PROTO_PATH = path.resolve(__dirname, './helloworld.proto');
 
 const app: Application = new Application(PROTO_PATH);
-app.start(`0.0.0.0:50051`);
+app.start(`127.0.0.1:50051`);
